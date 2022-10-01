4ee73995a67f8d5524fd88258fec08c74fa52593
LostLord07 <86511164+LostLord07@users.noreply.github.com>
2022-10-01 19:40:37 +0530

    Time: 3 ms (63.51%), Space: 6.3 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,54 +1,28 @@
 class Solution {
 public:
-    vector<int> dp;
+    // int ans=0;
     int numDecodings(string s) {
-        dp.resize(101,-1);
-        return solve(s,0);
+        if (s[0]=='0') return 0;
+        vector<int> dp(101,-1);
+        return solve(s,0,dp);
     }
     
-    int solve(string &s, int index)
-    {
+    int solve(string &s, int index, vector<int> &dp){
         if (index>=s.size()) return 1;
-        if (dp[index] != -1) return dp[index];
-        int res = 0;
-        int pickOne = s[index]-'0',pickTwo;
-        if (index<s.size()-1){
-          pickTwo = pickOne*10 + s[index+1]-'0';     
-        }
-    
-        if (pickOne>=1)res+=solve(s,index+1);
-        if (pickTwo>=10 && pickTwo<=26) res+=solve(s,index+2);
+        if (dp[index]!=-1)return dp[index];
+        int ans = 0;
         
-        return dp[index] = res;
+        int pickOne = s[index] - '0', pickTwo = 0;
+        if (index<s.size()-1){
+            if (pickOne>=1){
+                pickTwo = pickOne*10 + s[index+1]-'0'; 
+            }else return 0;
+        }
+        
+        if (pickOne>=1) ans+= solve(s,index+1,dp);
+        else return 0;
+        if (pickTwo>=10 && pickTwo<=26) ans+= solve(s,index+2,dp);
+        return dp[index] = ans;
         
     }
 };
-
-// class Solution {
-// public:int count = 0;
-//     bool selectOne(string s, int index)
-//     { if (s[index]!='0')return true; return false; }
-    
-    
-//     bool selectTwo(string s, int index1, int index2)
-//     {   if (index1<s.size() && index2<s.size()) {
-//             if (s[index1]=='1'||s[index1]=='2'){
-//                 if (s[index1]=='2' && s[index2]>='7') return false;
-//                 return true;
-//             }
-//         }return false;
-//     }
-    
-//     int numDecodings(string s) {
-//         solve(s,0);  return count;
-//         }
-    
-//     void solve(string s, int index)
-//     {
-//         if (index >= s.size()){count++; return;}
-//         if (selectOne(s,index)){solve(s,index+1);}
-//         if (selectTwo(s,index,index+1)){solve(s,index+2);}
-//     }
-    
-    
-// };
