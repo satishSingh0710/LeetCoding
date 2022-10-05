62158f2fb51065cd3f619bbd13982ea572477cdd
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-10-05 19:05:35 +0530

    Time: 7 ms (20.97%), Space: 8.7 MB (85.50%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        bool all_9 = true;
-        for (auto &i: digits){
-            if (i!=9){
-                all_9 = false;
-            }
-        }
+//         bool all_9 = true;
+//         for (auto &i: digits){
+//             if (i!=9){
+//                 all_9 = false;
+//             }
+//         }
         
-        if (all_9){
-            digits.insert(digits.begin(),1);
-            for (auto &i: digits){
-                i = 0;
-            }
-            digits[0] = 1;
-        }else{
+//         if (all_9){
+//             digits.insert(digits.begin(),1);
+//             for (auto &i: digits){
+//                 i = 0;
+//             }
+//             digits[0] = 1;
+//         }else{
             for (int i = digits.size()-1;i>=0;i--){
                 digits[i]++;
                 if(digits[i]==10){
                     digits[i] = 0;
                 }else break;
             }
+        // }
+        if (digits[0] == 0){
+            digits.insert(digits.begin(), 1);
         }
         
         return digits;
