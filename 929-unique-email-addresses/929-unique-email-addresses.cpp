053a02b0690b4ce0fc72ea3d5927847e2dec034e
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-10-05 19:25:42 +0530

    Time: 547 ms (5.04%), Space: 13.6 MB (82.13%) - LeetHub

---
+++ 929-unique-email-addresses/929-unique-email-addresses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        set<string> res;
+        for (int i = 0;i<emails.size();i++){
+            string mail = ""; bool foundPlus = false,foundAt = false;
+            for (int j = 0;j<emails[i].size();j++){
+                cout<<mail<<endl;
+                char c = emails[i][j];
+                if (c=='.' && foundAt == false)continue;
+                if (c=='+'){
+                   foundPlus = true;
+                } if (c=='@'){
+                       mail.push_back(c);
+                   foundPlus = false;
+                   foundAt = true;
+                }else{
+                    if (foundPlus==false)
+                    mail.push_back(c);
+                }
+            }
+            res.insert(mail);
+        }
+        return res.size();
+    }
+};
