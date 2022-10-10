36ebd51c2a256c43e3ba6b7a7a066535cfc49ed0
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-10-10 06:41:36 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     string breakPalindrome(string p) {
         if (p.size()==1) return "";
-        int n = p.size();   bool changed = false;     
+        int n = p.size();      
         for (int i = 0;i<n;i++)
         {
             if (p[i]=='a') continue;
             if (n%2==1 && i == n/2) continue;
             if (p[i]!='a'){
-               p[i] = 'a'; changed = true; return p;
+               p[i] = 'a'; return p;
             }
         }
         
-        if (!changed){p[n-1] = 'b';}
+        p[n-1] = 'b';
         return p;
     }
 };
