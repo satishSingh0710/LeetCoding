61a5e925f4ffb5658653d692825fcc01f44a49f7
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-10-13 13:10:24 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (67.29%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -1,21 +1,22 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-    TreeNode* removeLeafNodes(TreeNode* root, int target) {
-      if (root->left) root->left = removeLeafNodes(root->left, target);
-      if (root->right) root->right = removeLeafNodes(root->right, target); 
-    
-    return (!root->left && !root->right && root->val==target)?NULL:root;
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root.left!=null) root.left = removeLeafNodes(root.left,target);
+        if (root.right!=null) root.right = removeLeafNodes(root.right,target);
+        return (root.left==null && root.right==null && root.val == target)?null:root;
     }
- 
-};
+}
