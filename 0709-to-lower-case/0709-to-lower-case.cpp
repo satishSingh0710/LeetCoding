4b96404385a110327ff60eed48ef3bd99cc61954
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-10-16 19:21:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (82.15%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i = 0;i<s.size();i++){
+            if ((s[i]-0>=65) && (s[i]-0<=90)){
+                s[i]+= 32;
+            }
+        }
+        
+        return s;
+    }
+};
