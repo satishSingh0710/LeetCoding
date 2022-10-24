4aa0fe5a07ca09346feab786365af5616ef04770
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-10-24 18:57:16 +0530

    Time: 60 ms (36.06%), Space: 12.2 MB (22.00%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     string res = "";
     string minRemoveToMakeValid(string s) {
-        unordered_map<int, bool> mp;
         stack<int> st;
         for (int i = 0;i<s.size();i++){
-            if (st.size() == 0 && s[i] == ')') {mp[i] = true;}
+            if (st.size() == 0 && s[i] == ')') {s[i] = '*';}
             else if (s[i] == ')'){
                 st.pop();
             } else if (s[i] == '('){
@@ -14,13 +13,13 @@ public:
         }
         
         while (st.size()>0){
-            mp[st.top()] = true;
+            s[st.top()] = '*';
             st.pop();
         }
         
-        for (int i = 0;i<s.size();i++){
-            if (mp[i] == true) continue;
-            res+= s[i];
+        for(int i = 0;i<s.size();i++){
+            if (s[i] == '*')continue;
+            res += s[i];
         }
         return res;
     }
