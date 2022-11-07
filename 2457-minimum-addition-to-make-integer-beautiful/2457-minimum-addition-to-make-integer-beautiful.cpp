3451d1f2979321c9b55caf31d2f54149cd7a29f0
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-11-07 19:25:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (84.52%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -8,6 +8,10 @@ private:
         }
         return res;
     }
+    
+    // Day after tomorrow lena merese 
+    // I'm on social media detox, by god's will. 
+    // No what's app for today and tomorrow, and if there's anything important, then mail me.
 public:
     long long makeIntegerBeautiful(long long n, int target) {
         long long  base = 1, number = n;
