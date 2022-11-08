4c526dc33650be21bda9fb9e71d5c00725138c27
Satish Singh <86511164+LostLord07@users.noreply.github.com>
2022-11-08 18:10:35 +0530

    Time: 3 ms (90.25%), Space: 7.9 MB (46.04%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int dp[101][101];
-    int solve(vector<vector<int>> &obstacleGrid, int row, int col){
-        if (row == 0 && col == 0) {
-            if (obstacleGrid[row][col] == 0) 
-             return 1;
-             return 0;
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        bool foundStone = false;
+        for (int i = 0;i<m;i++){
+            if (obstacleGrid[i][0] == 1){foundStone = true;}
+            if (foundStone == false){
+                dp[i][0] = 1;
+            }
         }
-        if (row < 0  || col < 0) return 0;
-        if (obstacleGrid[row][col] == 1) return 0;
-        if (dp[row][col] != -1) return dp[row][col];
-        int top = solve(obstacleGrid, row-1, col);
-        int left = solve(obstacleGrid, row , col-1);
-        return dp[row][col] = top + left;
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid){
-        int n = obstacleGrid.size()-1, m = obstacleGrid[0].size()-1;
-        memset(dp, -1, sizeof(dp));
-        return solve(obstacleGrid, n, m);
+        foundStone = false;
+        for (int i = 0;i<n;i++){
+            if (obstacleGrid[0][i] == 1){foundStone = true;}
+            if (foundStone == false){
+                dp[0][i] = 1;
+            }
+        }
+        
+        
+        for (int i = 1;i<m;i++){
+            for (int j = 1;j<n;j++){
+                if (obstacleGrid[i][j] ==1) continue;
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
