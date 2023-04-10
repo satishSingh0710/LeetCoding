2ac9288fb58942808aa8a1b3d18d9eb316991e74
Satish Singh <86511164+satishSingh0710@users.noreply.github.com>
2023-04-10 23:34:54 +0530

    Time: 783 ms (8.30%), Space: 151.4 MB (8.16%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    int solve(int amount, vector<int> &store, int index,vector<vector<int>> &dp){
-        if (index == 0){
-            return amount;
-        }
-        if (dp[index][amount] != -1) return dp[index][amount];
-        int pick = INT_MAX, notPick = 0;
-        if (store[index] <= amount){
-            pick = 1 + min(solve(amount - store[index], store, index,dp), solve(amount - store[index], store, index-1,dp));
-        }
-        notPick = 0 + solve(amount, store, index-1,dp);
-        // cout<<pick<<" "<<notPick<<endl;
-        return dp[index][amount] = min(pick, notPick);
-    }
+    // int solve(int amount, vector<int> &store, int index,vector<vector<int>> &dp){
+    //     if (index == 0){
+    //         return amount;
+    //     }
+    //     if (dp[index][amount] != -1) return dp[index][amount];
+    //     int pick = INT_MAX, notPick = 0;
+    //     if (store[index] <= amount){
+    //         pick = 1 + min(solve(amount - store[index], store, index,dp), solve(amount - store[index], store, index-1,dp));
+    //     }
+    //     notPick = 0 + solve(amount, store, index-1,dp);
+    //     // cout<<pick<<" "<<notPick<<endl;
+    //     return dp[index][amount] = min(pick, notPick);
+    // }
     int numSquares(int n) {
         int maxNum = floor(sqrt(n));
-        vector<int> storeSqrs;
+        vector<int> store;
         for(int i = 1;i<=maxNum;i++){
-            storeSqrs.push_back(i*i);
+            store.push_back(i*i);
         }
-        vector<vector<int>> dp(storeSqrs.size(), vector<int>(n+1, -1));
-        return solve(n, storeSqrs, storeSqrs.size()-1, dp);
+        
+        vector<vector<int>> dp(store.size(), vector<int>(n+1));
+        for (int i = 0;i<store.size();i++){
+            for (int j = 0;j<=n; j++){
+                if (i == 0){
+                    dp[i][j] = j;
+                    continue;
+                }
+                int pick = INT_MAX, notPick = 0;
+                if (store[i] <= j){
+                    pick= 1 + min(dp[i][j - store[i]], dp[i-1][j - store[i]]);
+                }
+                notPick = 0 + dp[i-1][j];
+                dp[i][j] = min(pick, notPick);
+            }
+        }
+        return dp[store.size()-1][n];
         
     }
 };
