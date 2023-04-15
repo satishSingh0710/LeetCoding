fbb917cc91bd8062143f19f5e71bdfe278921693
Satish Singh <86511164+satishSingh0710@users.noreply.github.com>
2023-04-15 10:06:11 +0530

    Time: 226 ms (21.23%), Space: 63.8 MB (9.87%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> adj[graph.size()], revAdj[graph.size()];
-        vector<int> safe, inDegree(graph.size(), 0); 
+        int size = graph.size();
+        vector<int> adj[size], revAdj[size];
+        vector<int> safe, inDegree(size, 0); 
         queue<int> q;
-        for(int i = 0;i<graph.size();i++){
+        for(int i = 0;i<size;i++){
             adj[i] = graph[i];
         }
         
-        for (int i = 0;i<graph.size();i++){
+        for (int i = 0;i<size;i++){
             for (auto  &j: adj[i]){
                 revAdj[j].push_back(i);
             }
         }
-        for (int i = 0;i<graph.size();i++){
+        for (int i = 0;i<size;i++){
             for (auto &j: revAdj[i]){
                 inDegree[j]++;
             }
         }
-        for (int i = 0;i<graph.size();i++){
+        for (int i = 0;i<size;i++){
             if (inDegree[i] == 0)q.push(i);
         }
         
