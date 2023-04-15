6fdd91a30852a418c502404eaac33a3166a61012
Satish Singh <86511164+satishSingh0710@users.noreply.github.com>
2023-04-15 17:45:59 +0530

    Time: 52 ms (90.56%), Space: 42.1 MB (32.25%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{resolve()}, millis);
+    })
+
+}
+
+// async function sleep(millis) {
+//    return new Promise((resolve,reject)=>{
+//        setTimeout(()=>{
+//           resolve(1);
+//        },millis);
+//    })
+// }
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
