1a97582d6c940bb8c0116ad49cf6004a5f8e7d5f
Satish Singh <86511164+satishSingh0710@users.noreply.github.com>
2023-04-17 09:47:52 +0530

    Time: 4 ms (61.53%), Space: 9 MB (64.81%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        int maxi = *max_element(candies.begin(),candies.end());
+        int maxxCandies = INT_MIN;
         vector<bool> res;
         for (auto &i: candies){
-            if (i+extraCandies >= maxi){res.push_back(true);}
-            else res.push_back(false);
+            maxxCandies = max(maxxCandies, i);
+        }
+        for(int i = 0;i<candies.size();i++){
+            if (candies[i] + extraCandies >= maxxCandies){
+                res.push_back(true); 
+            }else{
+                res.push_back(false);
+            }
         }
         return res;
     }
