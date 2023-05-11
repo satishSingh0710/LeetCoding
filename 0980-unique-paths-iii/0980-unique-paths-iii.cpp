a6623da09b2607d60af1fa5efdcbf5b54de09291
Satish Singh <86511164+satishSingh0710@users.noreply.github.com>
2023-05-11 09:51:55 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (30.19%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,54 +1,34 @@
 class Solution {
-private: 
-    int endRow, endCol, result = 0, empty = 0;
-    int rows, cols;
 public:
-    void dfs(vector<vector<int>> &grid, vector<vector<bool>> &visited, int row, int col, int count){
-       
-        if (row == endRow && col== endCol){ 
-            count--;
-            if (count == empty)
-            { result++;}
-            return;
+    int solve(vector<vector<int>> &grid, int row, int col, int &eRow, int &eCol,int &m, int &n, int pathLength,int &empty,vector<vector<bool>> &visited, vector<vector<int>> &dp){
+        if (row<0||col<0||row>=m||col>=n||visited[row][col] || grid[row][col] == -1) return 0; 
+        if (row == eRow && col==eCol){
+            if (pathLength-1 == empty){return 1;}
+            return 0; 
         }
-        if (row<0 || col<0 || row>=rows || col>=cols){
-          return;
-        }
-        if (grid[row][col] == -1) return;
-        if (visited[row][col] == true) return;
-        
-        
-        visited[row][col] = true;
-        
-        dfs(grid, visited, row+1, col, count+1);
-        dfs(grid, visited, row, col+1, count+1);
-        dfs(grid, visited, row-1, col, count+1);
-        dfs(grid, visited, row,col-1, count+1);
+        // if (dp[row][col]!=-1) return dp[row][col];
+        visited[row][col] = true; 
+        int ans = 0;
+        ans = ans + solve(grid, row+1, col, eRow, eCol, m,n,pathLength+1,empty, visited, dp); 
+        ans = ans+ solve(grid, row-1, col, eRow, eCol, m, n, pathLength +1, empty, visited,dp); 
+        ans += solve(grid, row, col+1, eRow, eCol, m, n, pathLength+1, empty, visited, dp); 
+        ans += solve(grid, row, col-1, eRow, eCol, m, n, pathLength +1, empty, visited, dp);
         visited[row][col] = false;
-        
+        return  ans; 
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
-       
-       int n = grid.size(), m = grid[0].size(), startRow, startCol;
-        rows = n, cols = m;
-       vector<vector<bool>> visited(n, vector<bool>(m, false));
-       
-        for (int i = 0;i<n;i++){
-            for (int j = 0;j<m;j++){
-                if (grid[i][j] == 0){
-                   empty++;
-                }
-                if (grid[i][j] == 1){
-                    startRow = i; startCol = j;
-                }
-                if (grid[i][j] == 2){
-                    endRow = i; endCol = j;
-                }
+        int countEmptySpaces = 0;
+        int m=grid.size(), n = grid[0].size();
+        int sR =0, sC= 0, eR=0, eC=0;
+        for (int i = 0;i<grid.size();i++){
+            for (int j = 0;j<grid[0].size();j++){
+                if(grid[i][j] == 0){countEmptySpaces++;}
+                else if (grid[i][j] == 1){sR = i; sC = j;}
+                else if (grid[i][j] == 2){eR  = i; eC = j;}
             }
         }
-
-        dfs(grid, visited, startRow, startCol,0);
-        return result;
-      
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<bool>> visited(m, vector<bool> (n, false)); 
+        return solve(grid,sR,sC,eR,eC,m,n,0,countEmptySpaces,visited,dp);
     }
 };
